c78b63f291af18b51e7dac56d744a9c6afa5dcec
DrishttiNarwal <drishtti.narwal@gmail.com>
2025-09-21 04:24:52 +0530

    Deleted contact page

--- frontend/nyayabot-app/src/App.js
+++ frontend/nyayabot-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Chat from './pages/Chat';
-import Contact from './pages/Contact';
 import Home from './pages/Home';  
 import Summarize from './pages/Summarize';
 
@@ -13,8 +12,8 @@ function App() {
           <h1 className="title">NyayaBot</h1>
           <nav className="nav-links">
             <Link to="/">Home</Link>
+            <Link to="/Chat">Chat</Link>
             <Link to="/Summarize">Summarize</Link>
-            <Link to="/Contact">Contact</Link>
           </nav>
         </header>
 
@@ -24,7 +23,6 @@ function App() {
             {/* Add other routes below */}
             <Route path="/chat" element={<Chat />} />
             <Route path="/summarize" element={<Summarize />} />
-            <Route path="/contact" element={<Contact />} />
             {/* Add /services if you want */}
           </Routes>
         </main>
